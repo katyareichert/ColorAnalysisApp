6ecf8562684837fb732990eda34cddb2ca939701
katyareichert <katerinamreichert@gmail.com>
2024-04-16 17:42:22 -0400

    2 column info page

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def drag_and_drop():
         "title": "Undertone",
         "class1": "COOL",
         "class2": "WARM",
-        "colors": ["f52c10ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
+        "colors": ["f53310ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
     }
 
     return render_template('drag_and_drop.html', data=data)
@@ -53,11 +53,33 @@ def reorder():
         "title": "Chroma",
         "class1": "MUTED",
         "class2": "SATURATED",
-        "colors": ["f52c10ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
+        "colors": ["f53310ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
 # "colors": ["8f6190ff", "797979ff", "b055b1ff", "887086ff", "c44acaff", "9e5ca1ff"] real colors
     }
 
     return render_template('reorder.html', data=data)
+
+@app.route('/cc')
+def color_columns():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
+    data = {
+        "title": "Undertone",
+        "class1": "COOL",
+        "class2": "WARM",
+        "colors1": ["f53310ff", "ca3b1eff", "fc5f47ff"],
+        "colors2": ["bd0c42ff", "fa2f56ff", "d72942ff"],
+        "description1": """A cool red leans more towards purple, 
+            suggesting calmness, depth, and similarity to ripe cranberries.""",
+        "description2": """A warm red has hints of orange, embodying vibrancy, 
+            energy, and resembling a fiery sunset."""
+    }
+
+    return render_template('color_columns.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug = True)
