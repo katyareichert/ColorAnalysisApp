6c797f96f26028a43456f7b45540204d766a5aa8
katyareichert <katerinamreichert@gmail.com>
2024-04-16 14:36:37 -0400

    Setup drag and drop template

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ColorAnalysisApp
 Learn Color Analysis in 10 Minutes
+
+### Undertone
+The subtle hue or color that lies beneath the surface of a primary color, influencing its overall appearance.
+
+### Value
+The degree of lightness or darkness of a color, independent of its hue or chroma.
+
+### Chroma
+The purity or intensity of a color, representing its saturation or vividness relative to a neutral gray of the same lightness.
--- server.py
+++ server.py
@@ -24,5 +24,22 @@ def welcome():
     """
     return render_template('index.html')
 
+@app.route('/dnd')
+def drag_and_drop():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
+    data = {
+        "title": "Undertone",
+        "class1": "COOL",
+        "class2": "WARM",
+        "colors": ["f52c10ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
+    }
+
+    return render_template('drag_and_drop.html', data=data)
+
 if __name__ == '__main__':
     app.run(debug = True)
