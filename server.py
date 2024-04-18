2e2497473da17e564e14b8918d6c4b0a3ba83a5d
katyareichert <katerinamreichert@gmail.com>
2024-04-18 18:52:10 -0400

    Start adding sequence

--- server.py
+++ server.py
@@ -27,22 +27,14 @@ def learn(module, lesson):
 
     Returns: render template
     """
+    if module not in lesson_items:
+        return "Error 404: Page does not exist"
 
-    if module == "undertone":
-        return undertone(lesson)
-    if module == "value":
-        return value(lesson)
-    if module == "chroma":
-        return chroma(lesson)
-
-    return "Error 404: Page does not exist"
-
-quiz_items = [
-    ("", {
-        "question": "",
-        "media": []
-    })
-]
+    try:
+        render_page, data = lesson_items[module][int(lesson)]
+        return render_page(data)
+    except ValueError:
+        return "Error 404: Page does not exist"
 
 @app.route('/quiz/<question>')
 def quiz(question):
@@ -54,121 +46,161 @@ def quiz(question):
 
     return render_template(quiz_items[question][0], data = quiz_items[question][1])
 
-# MODULES - HELPER ROUTES ------------------------------
-
-def undertone(lesson):
-    """
-    Undertone Module
-    """
-    return 0
-
-def value(lesson):
-    """
-    Value Module
-    """
-    return 0
-
-def chroma(lesson):
-    """
-    Chroma Module
-    """
-    return 0
-
-# HIDDEN TEST ROUTES ------------------------------------
+# HIDDEN HELPER ROUTES ------------------------------------
 
 @app.route('/mod')
-def module_subheader(
-    title = "Undertone",
-    description = "The subtle hue or color that lies beneath the surface of a primary color, influencing its overall appearance.",
-    left_img = "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg",
-    right_img = "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg"
-):
+def mod():
+    """
+    TEST
+    """
+    return module_subheader(data = {
+            "title": "Undertone",
+            "description": """The subtle hue or color that lies beneath the surface
+                of a primary color, influencing its overall appearance.""",
+            "left_img": 
+                "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg",
+            "right_img": 
+                "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg",
+        })
+def module_subheader(data):
     """
     Module section header test page
 
     Returns: render template
     """
-
-    data = {
-        "title": title,
-        "description": description,
-        "left": left_img,
-        "right": right_img
-    }
-
     return render_template('subheader.html', data=data)
 
 @app.route('/dnd')
-def drag_and_drop(
-    colors,
-    title = "Undertone",
-    class1 = "COOL",
-    class2 = "WARM"
-):
+def dnd():
+    """
+    TEST
+    """
+    return drag_and_drop(data = {
+            "title": "Undertone",
+            "class1": "COOL",
+            "class2": "WARM",
+            "colors": ["f53310ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
+        })
+def drag_and_drop(data):
     """
     Drag and Drop activity test page
 
     Returns: render template
     """
-
-    if not colors or colors == []:
-        colors = ["f53310ff", "bd0c42ff", "fc5f47ff", "ca3b1eff", "fa2f56ff", "d72942ff"]
-
-    data = {
-        "title": title,
-        "class1": class1,
-        "class2": class2,
-        "colors": colors
-    }
-
     return render_template('drag_and_drop.html', data=data)
 
 @app.route('/reorder')
-def reorder(
-    colors,
-    title = "Chroma",
-    class1 = "MUTED",
-    class2 = "SATURATED"
-):
+def reor():
+    """
+    TEST
+    """
+    return reorder(data = {
+            "title": "Chroma",
+            "class1": "MUTED",
+            "class2": "SATURATED",
+            "colors": ["8f6190ff", "797979ff", "b055b1ff", "887086ff", "c44acaff", "9e5ca1ff"]
+        })
+def reorder(data):
     """
     Reorder activity test page
 
     Returns: render template
     """
-
-    if not colors or colors == []:
-        colors = ["8f6190ff", "797979ff", "b055b1ff", "887086ff", "c44acaff", "9e5ca1ff"]
-
-    data = {
-        "title": title,
-        "class1": class1,
-        "class2": class2,
-        "colors": colors
-    }
-
     return render_template('reorder.html', data=data)
 
 @app.route('/cc')
-def color_columns():
+def cc():
+    """
+    TEST
+    """
+    return color_columns(data = {
+            "title": "Undertone",
+            "class1": "COOL",
+            "class2": "WARM",
+            "colors1": ["bd0c42ff", "fa2f56ff", "d72942ff"],
+            "colors2": ["f53310ff", "ca3b1eff", "fc5f47ff"],
+            "description1": """A cool red leans more towards purple, 
+                suggesting calmness, depth, and similarity to ripe cranberries.""",
+            "description2": """A warm red has hints of orange, embodying vibrancy, 
+                energy, and resembling a fiery sunset."""
+        })
+def color_columns(data):
     """
     Two Column information test page
 
     Returns: render template
     """
-
-    data = {
-        "title": "Undertone",
-        "class1": "COOL",
-        "class2": "WARM",
-        "colors1": ["bd0c42ff", "fa2f56ff", "d72942ff"],
-        "colors2": ["f53310ff", "ca3b1eff", "fc5f47ff"],
-        "description1": """A cool red leans more towards purple, 
-            suggesting calmness, depth, and similarity to ripe cranberries.""",
-        "description2": """A warm red has hints of orange, embodying vibrancy, 
-            energy, and resembling a fiery sunset."""
-    }
-
     return render_template('color_columns.html', data=data)
+
+# DATA
+
+lesson_items = {
+    "undertone": [
+        (module_subheader, {
+            "title": "Undertone",
+            "description": """The subtle hue or color that lies beneath the
+                surface of a primary color, influencing its overall appearance.""",
+            "left_img": 
+                "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg",
+            "right_img": 
+                "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg"
+        }),
+        (drag_and_drop, {
+            "title": "Undertone",
+            "class1": "COOL",
+            "class2": "WARM",
+            "colors": ["bd0c42ff", "fa2f56ff", "d72942ff", "f53310ff", "ca3b1eff", "fc5f47ff"]
+        }),
+        (color_columns, {
+            "title": "Undertone",
+            "class1": "COOL",
+            "class2": "WARM",
+            "colors1": ["bd0c42ff", "fa2f56ff", "d72942ff"],
+            "colors2": ["f53310ff", "ca3b1eff", "fc5f47ff"],
+            "description1": """A cool red leans more towards purple, 
+                suggesting calmness, depth, and similarity to ripe cranberries.""",
+            "description2": """A warm red has hints of orange, embodying vibrancy, 
+                energy, and resembling a fiery sunset.""",
+            "long1": """Cool colors evoke a peaceful atmosphere, inviting exploration
+                with a newfound sense of clarity and wonder.""",
+            "long2": """Warm colors awaken a feeling of energy and vibrancy, igniting
+                a newfound appreciation for the richness of the world."""
+        })
+    ],
+    "value": [
+        (module_subheader, {
+            "title": "Value",
+            "description": """The subtle hue or color that lies beneath the
+                surface of a primary color, influencing its overall appearance.""",
+            "left_img": 
+                "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg",
+            "right_img": 
+                "https://cdn.vectorstock.com/i/500p/20/11/color-whell-to-white-vector-2992011.jpg"
+        }),
+        (color_columns, {
+            "title": "Value",
+            "class1": "COOL",
+            "class2": "WARM",
+            "colors1": ["bd0c42ff", "fa2f56ff", "d72942ff"],
+            "colors2": ["f53310ff", "ca3b1eff", "fc5f47ff"],
+            "description1": """A cool red leans more towards purple, 
+                suggesting calmness, depth, and similarity to ripe cranberries.""",
+            "description2": """A warm red has hints of orange, embodying vibrancy, 
+                energy, and resembling a fiery sunset.""",
+            "long1": """Cool colors evoke a peaceful atmosphere, inviting exploration
+                with a newfound sense of clarity and wonder.""",
+            "long2": """Warm colors awaken a feeling of energy and vibrancy, igniting
+                a newfound appreciation for the richness of the world."""
+        })
+    ]
+}
+
+quiz_items = [
+    ("thisorthat.html", {
+        "question": "",
+        "media": []
+    })
+]
 
 if __name__ == '__main__':
     app.run(debug = True)
